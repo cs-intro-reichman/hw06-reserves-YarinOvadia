395efd0367c69c664a25ed8a4fcf21ac6b09ec89
Yarin Ovadia <yarin.ovadia@post.runi.ac.il>
2024-03-06 23:03:34 +0200

    Update Editor4

--- Editor4.java
+++ Editor4.java
@@ -3,5 +3,14 @@ import java.awt.Color;
 public class Editor4 {
 
 	public static void main (String[] args) {
+		String filename = args[0];
+		int n = Integer.parseInt(args[1]);
+
+
+		Color[][] image1 = Runigram.read(filename);
+		Color[][] image2 = Runigram.grayScaled(image1);
+
+		Runigram.morph(image1,image2,n);
+
 	}
-	
+}
